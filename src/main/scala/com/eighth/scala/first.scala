26729f5b30d97fe88fa996c353728e9664cd1e85
dhananjayaaps <dhananjayaaps@gmail.com>
2023-08-15 07:58:14 +0530

    eighth week

--- src/main/scala/com/eighth/scala/first.scala
+++ src/main/scala/com/eighth/scala/first.scala
@@ -2,23 +2,22 @@ package com.eighth.scala
 
 object first {
 
-  def main(args: Array[String]): Unit = {
-      def calculateInterest(depositAmount: Double): Double = {
-        val interestRate: Double => Double = {
-          case amount if amount <= 20000 => 0.02
-          case amount if amount <= 200000 => 0.04
-          case amount if amount <= 2000000 => 0.035
-          case _ => 0.065
-        }
-
-        val interest = depositAmount * interestRate(depositAmount)
-        interest
-      }
-
-      def main(args: Array[String]): Unit = {
-        val depositAmount = 250000
-        val interest = calculateInterest(depositAmount)
-        println(s"For a deposit of Rs. $depositAmount, the interest earned is Rs. $interest")
-      }
+  def Interest(deposit: Double): Double = {
+    val interestRate: Double => Double = {
+      case amount if amount <= 20000 => 0.02
+      case amount if amount <= 200000 => 0.04
+      case amount if amount <= 2000000 => 0.035
+      case _ => 0.065
     }
+
+    val interest = deposit * interestRate(deposit)
+    interest
+  }
+
+  def main(args: Array[String]): Unit = {
+    val deposit = 250000
+    val interest = Interest(deposit)
+    println(s"For a deposit of Rs. $deposit, the interest earned is Rs. $interest")
+  }
+
 }
