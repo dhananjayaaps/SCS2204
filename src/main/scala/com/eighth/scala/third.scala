9ae0f19a18fa23f15bdd7be31a4db1253838f84a
sineth <dhananjayaaps@gmail.com>
2023-08-22 07:40:26 +0530

    ninth week

--- src/main/scala/com/eighth/scala/third.scala
+++ src/main/scala/com/eighth/scala/third.scala
@@ -2,27 +2,29 @@ package com.eighth.scala
 
 object Third {
 
+  def toUpper: String => String = input => input.toUpperCase
+
+  def toLower: String => String = input => input.toLowerCase
+
+  def capitalizeAtIndex: (String, Int) => String = (input, index) => {
+    val before = input.substring(0, index)
+    val after = input.substring(index + 1)
+    val charToCapitalize = input.charAt(index).toUpper
+
+    before + charToCapitalize + after
+  }
+
+  def formatNames(name: String)(format: String => String): String = {
+    format(name)
+  }
+
   def main(args: Array[String]): Unit = {
     val names = List("Benny", "Niroshan", "Saman", "Kumara")
-
-    def toUpper: String => String = input => input.toUpperCase
-    def toLower: String => String = input => input.toLowerCase
-
-    def capitalizeAtIndex: (String, Int) => String = (input, index) => {
-      val before = input.substring(0, index)
-      val after = input.substring(index + 1)
-      val charToCapitalize = input.charAt(index).toUpper
-
-      before + charToCapitalize + after
-    }
 
     names.foreach(name => println(formatNames(name)(toUpper)))
     names.foreach(name => println(formatNames(name)(toLower)))
     names.foreach(name => println(formatNames(name)(capitalizeAtIndex(_, 2))))
   }
 
-  def formatNames(name: String)(format: String => String): String = {
-    format(name)
-  }
 
 }
